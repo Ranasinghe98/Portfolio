fa826dc78a5c4bff68fed76a318f800ac553351d
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2022-12-11 01:19:30 +0530

    remove test section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,6 @@ function Header(){
             <div className="col-12 col-md-12 col-sm-12 col-xs-12 header">
                 <img alt="logo" src={Logo} className="header-logo" />
             </div>
-            <div className="test"></div>
         </>
         )
 }
