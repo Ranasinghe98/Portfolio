666cd9af06513fb13bc67d8cd4d56c5c4cb5fc5c
Roshitha Ranasinghe <sajithroshitha98@gmail.com>
2022-12-11 01:17:52 +0530

    update slider

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,10 @@ function Header(){
 
     return(
         <>
-            <div className=" header">
+            <div className="col-12 col-md-12 col-sm-12 col-xs-12 header">
                 <img alt="logo" src={Logo} className="header-logo" />
             </div>
+            <div className="test"></div>
         </>
         )
 }
