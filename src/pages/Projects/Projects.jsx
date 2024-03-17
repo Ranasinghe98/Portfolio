7cf4e334af1e21c5672f76e1001c348e54a76379
unknown <sajithroshitha98@gmail.com>
2024-03-17 14:40:58 +0530

    modify changes

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import "./projects.css";
 import projectsInfo from "./projectsInfo.js";
 import gsap from "gsap"
 import ScrollTrigger from "gsap/ScrollTrigger";
@@ -8,48 +7,37 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 const Projects = () => {
 
     useEffect(() => {
-
-        document.title = "Projects | Dev Roshitha"
-
+      document.title = "Projects | Dev Roshitha"
     }, [])
 
     gsap.registerPlugin(ScrollTrigger)
 
     useEffect(() => {
-
-        gsap.from(".projects-box-wrapper", {
-            duration: 1
-        })
-
         gsap.from(".pro-box", {
             scrollTrigger: {
                 trigger: ".pro-box",
                 start: "top center",
                 toggleActions: "restart pause restart none",
             },
-            
-            duration: 1,
-            opacity: 0,
-            // ease: "bounce"
         })
     }, [])
 
     return(
         <>
         
-            <div className="row pro-box">
-                <div className="col-12 projectTitle">
-                    <hr/>
-                    <h2>Recent <span className="style-word">Projects</span></h2>
+            <div className="pro-box">
+                <div className="contentTitle titleHead">
+                  <hr className="title-line"/>
+                  <h2 className="light-heading">Recent <span>Projects</span></h2>
                 </div>
             </div>
             
 
 
-            <div className="row projects-box-wrapper ">
+            <div className="projects-box-wrapper ">
                 {projectsInfo.map((values)=>{
                     return(
-                        <div className="col-sm-12 col-md-4 col-lg-4 project-box pro-box">
+                        <div className="project-box pro-box">
                             <img className="box-img" src={values.imgSrc} alt={values.altText}/>
                             <h3>{values.title}</h3>
                             <p>{values.content}</p>
